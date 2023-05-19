a64c0ed3fe3575426ed4cdcbd2346644ca2f6b01
ReArmel <rearmel@icloud.com>
2023-05-19 16:44:50 -0400

    review and adjustment

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -32,11 +32,5 @@ namespace SalesWebMvc.Controllers
             var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
             return View(result);
         }
-
-        public IActionResult GroupingSearch()
-        {
-            return View();
-        }
-
     }
 }
